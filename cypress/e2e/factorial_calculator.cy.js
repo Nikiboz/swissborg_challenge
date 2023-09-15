987564fe54906e0776e59b5d91477b2151e9d9c4
Nikiboz <nick.tkachuk@gmail.com>
2023-09-15 14:40:38 +0100

    final version

--- cypress/e2e/factorial_calculator.cy.js
+++ cypress/e2e/factorial_calculator.cy.js
@@ -30,62 +30,53 @@ function factorial(n) {
   return result;
 }
 
-
-describe('Factorial calculator', () => {
+describe("Factorial calculator", () => {
   //array 10-100
   const Input_integers = Array.from({ length: 91 }, (_, i) => i + 10);
 
+  for (const number of Input_integers) {
+    //moving this FOR will drasticly increase the performace
+    it(`Calculating factorial for ${number}`, () => {
+      let server_result;
+      cy.visit("https://qainterview.pythonanywhere.com/", {
+        onBeforeLoad(win) {
+          cy.spy(win.console, "log").as("consoleLog");
+        },
+      });
 
- for (const item of Input_integers) {                                                 //moving this FOR will drasticly increase the performace
-  it(`90 cases of calculation factorial for ${item}`, () => {
-    let server_result
-    cy.visit('https://qainterview.pythonanywhere.com/', {
-      onBeforeLoad(win) {
-        cy.spy(win.console, 'log').as('consoleLog')
-      }
-    })
-    
-    
-    //iterates over array
-      
-      cy.intercept('https://qainterview.pythonanywhere.com/factorial').as('reply') //intercepts API requests 
-      cy.get('#number').clear().type(item) //Get's input field and types integer from array
-      cy.get('#getFactorial').click() //Runs the calculation
-     
-      cy.wait('@reply');
-     
-      cy.get('@reply').should((response) => {
+      //iterates over array
+
+      cy.intercept("https://qainterview.pythonanywhere.com/factorial").as(
+        "reply"
+      ); //intercepts API requests
+      cy.get("#number").clear().type(item); //Get's input field and types integer from array
+      cy.get("#getFactorial").click(); //Runs the calculation
+
+      cy.wait("@reply");
+
+      cy.get("@reply").should((response) => {
         console.log(response.body); // Log the response body
-        
-        expect(response.response.body).to.have.property('answer');
+
+        expect(response.response.body).to.have.property("answer");
         server_result = response.response.body.answer;
-  // Now you can use server_result as needed
+        // Now you can use server_result as needed
       });
 
-      cy.get('@consoleLog').should('be.calledWith', String(item)) //looks for calls on calculator console
-      cy.get('@consoleLog').should('be.calledWith', "Hello! I am in the done part of the ajax call")  //Verifies that calculations are made
+      var invalid_calculations = [];
+      const expected = factorial(number);
+      if (server_result !== expected) {
+        invalid_calculations.push(
+          `❌ factorial result for ${number} is wrong, expected ${expected}, got ${server_result} from network`
+        );
+        cy.log(
+          `❌ factorial result for ${number} is wrong, expected ${expected}, got ${server_result}.`
+        );
+      }
+    });
 
-      var invalid_calculations = []
-      cy.get('@consoleLog').then(spy => {
-        const calls = spy["getCalls"](); 
-        const { args: [{answer: serverResult}] } = calls[calls.length - 1];
-     
-        const expected = factorial(item)
-        if (serverResult !== expected || server_result !== expected) {
-         
-         invalid_calculations.push(`❌ factorial result for ${item} is wrong, expected ${expected}, got ${serverResult} from console and ${server_result} from network`)
-          cy.log(`❌ factorial result for ${item} is wrong, expected ${expected}, got ${serverResult}. Server responded with ${server_result} and ${server_result} from network`)
-        
-        }
-        spy.resetHistory()
-        
-   
-      });
-
-      console.log(invalid_calculations)
-    
-    cy.writeFile('invalid_calculations_list.txt', invalid_calculations, { flag: 'a+' })
-    cy.wrap(invalid_calculations).should('be.empty')
-  })
+    cy.writeFile("invalid_calculations_list.txt", invalid_calculations, {
+      flag: "a+",
+    });
+    cy.wrap(invalid_calculations).should("be.empty");
   }
-})
+});
